bbd43ee45b8cc1b16951e91c577c9949cafd81fe
m365m365 <139079678+m365m365@users.noreply.github.com>
2026-04-24 11:27:31 +0800

    add myfood database tab ai_restaurant_info

---
+++ src/main/java/com/example/topfood2604/entity/AiRestaurantInfo.java
@@ -0,0 +1,25 @@
+package com.example.topfood2604.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ai_restaurant_info")
+public class AiRestaurantInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String url;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Lob
+    @Column(name = "base64_image")
+    private String base64Image;
+
+    private String state;
+}
