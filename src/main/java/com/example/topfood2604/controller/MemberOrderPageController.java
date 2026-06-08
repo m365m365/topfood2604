902a48807fcfaacd165a6c84a139b7c3379b0622
m365m365 <139079678+m365m365@users.noreply.github.com>
2026-06-08 10:24:26 +0800

    假付款 假物流通知

--- src/main/java/com/example/topfood2604/controller/MemberOrderPageController.java
+++ src/main/java/com/example/topfood2604/controller/MemberOrderPageController.java
@@ -1,7 +1,9 @@
 package com.example.topfood2604.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MemberOrderPageController {
@@ -9,5 +11,14 @@ public class MemberOrderPageController {
     @GetMapping("/member-orders")
     public String memberOrdersPage() {
         return "member-orders";
+    }
+
+    @GetMapping("/fake-payment/{orderId}")
+    public String fakePaymentPage(
+            @PathVariable Long orderId,
+            Model model
+    ) {
+        model.addAttribute("orderId", orderId);
+        return "fake-payment";
     }
 }
